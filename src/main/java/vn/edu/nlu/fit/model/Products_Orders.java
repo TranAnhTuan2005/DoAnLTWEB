5d5d07118ce37f958ce3f12d37a20a66107b0e5f
Pham Thanh An-23130002 <23130002@st.hcmuaf.edu.vn>
2026-01-18 10:32:54 +0700

    Thêm tên sp trong product_orders cho dễ quản lý, chỉnh vài chi tiết

--- src/main/java/vn/edu/nlu/fit/model/Products_Orders.java
+++ src/main/java/vn/edu/nlu/fit/model/Products_Orders.java
@@ -4,17 +4,19 @@ public class Products_Orders {
     private int id;
     private int orderID;
     private int productID;
+    private String productName;
     private int quantity;
     private double priceAtTime;
 
     public Products_Orders() {
     }
 
-    public Products_Orders(int id, int orderID, int productID, int quantity, double priceAtTime) {
+    public Products_Orders(int id, int orderID, int productID, int quantity, String productName, double priceAtTime) {
         this.id = id;
         this.orderID = orderID;
         this.productID = productID;
         this.quantity = quantity;
+        this.productName = productName;
         this.priceAtTime = priceAtTime;
     }
 
@@ -58,12 +60,21 @@ public class Products_Orders {
         this.priceAtTime = priceAtTime;
     }
 
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
     @Override
     public String toString() {
         return "Products_Orders{" +
                 "id=" + id +
                 ", orderID=" + orderID +
                 ", productID=" + productID +
+                ", productName='" + productName + '\'' +
                 ", quantity=" + quantity +
                 ", priceAtTime=" + priceAtTime +
                 '}';
